0c87b5f9b4ae1fcdd88df52e387fde1a8aaf447d
Gustavo Marttos <marttosc@gmail.com>
2016-10-12 20:47:36 -0300

    Allows the use of other sites.

--- crawler.py
+++ crawler.py
@@ -1,24 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'http://tableless.com.br'
+url = input('Digite a URL do site a ser analisado: ')
+
+if url is None or url == '':
+    url = 'http://tableless.com.br'
+
 r = requests.get(url)
-soup = BeautifulSoup(r.text)
+soup = BeautifulSoup(r.text, 'html.parser')
 
-print('Voce esta executando uma crawler na pagina do Tableless!')
-response = input('Digite a tag a ser procurada no blog Tableless entre aspas: ')
+print('\nVocê está executando uma crawler na página do "' + soup.title.string + '"!\n')
 
-tag = [title.text for title in soup.findAll(response)]
+element_search = input('Digite a tag a ser procurada (e.g.: body, div, p): ')
 
+elements = [ element.text for element in soup.findAll(element_search) ]
 
+if len(elements) > 0:
+    print('\nElementos com a tag "' + element_search + '"...')
 
-if tag:
-	print('ITENS COM A TAG SELECIONADA')
-	for item in tag:
-	    print(item)
-else: 
-	print('ESSA TAG NAO EXISTE NESTA PAGINA')
-
-
-
-
+    for element in elements:
+        print(element)
+else:
+    print('\nTag não encontrada na página!')
